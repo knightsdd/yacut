f3d9d1e78eb85f20ef4619ba52cfe875b693972d
knightsdd <gorvas53@yandex.ru>
2022-06-26 18:23:28 +0300

    Complite view functiopn for create short urls

--- yacut/views.py
+++ yacut/views.py
@@ -1,6 +1,7 @@
+import os
 from random import randint
 
-from flask import render_template
+from flask import render_template, flash
 
 from . import app, db
 from .forms import UrlMapForm
@@ -33,8 +34,27 @@ def get_unique_short_id() -> str:
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = UrlMapForm()
-    # if form.validate_on_submit():
-
+    if form.validate_on_submit():
+        original_link = form.original_link.data
+        custom_id = form.custom_id.data
+        if custom_id == '':
+            custom_id = get_unique_short_id()
+        else:
+            if URL_map.query.filter_by(short=custom_id).first() is not None:
+                flash('Такая короткая ссылка уже занята!')
+                return render_template('urlsform.html', form=form)
+        
+        new_url = URL_map(
+            original=original_link,
+            short=custom_id)
+        db.session.add(new_url)
+        db.session.commit()
+        base_url = os.getenv('BASE_URL')
+        full_short_url = f'{base_url}{custom_id}'
+        return render_template(
+            'urlsform.html',
+            form=form,
+            short_url=full_short_url)
     return render_template('urlsform.html', form=form)
 
 
